4da23e3158ba56328c13212e57d2ba1260372f8d
Reenuay <reenuay777@gmail.com>
2021-09-18 03:09:54 +0400

    Add first migration

--- InsightClub.Db.Migrator/Program.fs
+++ InsightClub.Db.Migrator/Program.fs
@@ -1,3 +1,46 @@
+module Program
+
+open DbUp
+open System
+open System.IO
+open System.Reflection
+
+
 [<EntryPoint>]
 let main argv =
-  0 // return an integer exit code
+  let config =
+    #if DEBUG
+    "../../../" +
+    #endif
+    "config"
+
+  let connectionString = File.ReadAllText config
+
+  let upgrader =
+    DeployChanges
+      .To
+      .PostgresqlDatabase(connectionString)
+      .WithScriptsEmbeddedInAssembly(
+        Assembly.GetExecutingAssembly(),
+        fun p -> p.EndsWith ".psql")
+      .LogToConsole()
+      .Build()
+
+  let result = upgrader.PerformUpgrade()
+
+  if not result.Successful then
+    Console.ForegroundColor <- ConsoleColor.Red
+    Console.WriteLine result.Error
+    #if DEBUG
+    Console.ReadKey()
+    |> ignore
+    #endif
+    -1
+  else
+    Console.ForegroundColor <- ConsoleColor.Green
+    Console.WriteLine "Success!"
+    #if DEBUG
+    Console.ReadKey()
+    |> ignore
+    #endif
+    0
